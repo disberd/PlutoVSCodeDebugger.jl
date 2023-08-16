24640fe2715d641daf9598d205b4237e10c13215
Alberto Mengali <disberd@gmail.com>
2023-08-16 15:23:50 +0200

    add very simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using PlutoVSCodeDebugger
+using PlutoVSCodeDebugger: process_expr, check_pluto
 using Test
 
 @testset "PlutoVSCodeDebugger.jl" begin
-    # Write your tests here.
+    file = @__FILE__
+    ex = :(my_func(@__FILE__))
+    s = string(process_expr(ex, @__MODULE__, file))
+    @test !contains(s, "@__FILE__")
+    @test contains(s, file)
+
+    ex = :(my_func(@__MODULE__))
+    s = string(process_expr(ex, @__MODULE__, file))
+    @test !contains(s, "@__MODULE__")
+    @test contains(s, string(@__MODULE__))
+    
+    @test_logs (:warn, r"ignoring this command") check_pluto()
 end
